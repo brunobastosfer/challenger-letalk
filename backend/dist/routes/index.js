d0ef8521f4df2886168e61c9cfd50488eae0d5ab
brunobastosfer <brunobastos@edu.unifor.br>
2021-09-25 09:31:52 -0300

    rollback

--- backend/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-var express_1 = require("express");
-var users_routes_1 = require("./users.routes");
-var router = (0, express_1.Router)();
-exports.router = router;
-router.use("/users", users_routes_1.usersRoute);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-
 import { router } from './routes'
 
-import './database'
+// import './database'
 
 const app = express()
 
 app.use(cors())
-app.use(express.json());
-
 app.use(router)
 
-app.listen(process.env.PORT || 33330);
+app.listen(3333, () => console.log('Server running'))
